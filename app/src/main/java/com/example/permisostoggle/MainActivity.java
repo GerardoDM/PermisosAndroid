5ffef7090180eaed73b624eb6e8da5b14c806111
GerardoDM <geradelgado22@hotmail.com>
2020-03-22 18:54:43 -0600

    2nd commit

--- app/src/main/java/com/example/permisostoggle/MainActivity.java
+++ app/src/main/java/com/example/permisostoggle/MainActivity.java
@@ -12,22 +12,41 @@ import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CompoundButton;
+import android.widget.Switch;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     private int STORAGE_PERMISSION_CODE = 1;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.layout);
 
-        Button buttonRequest = findViewById(R.id.button);
+        // initiate a Switch
+        Switch switchOne = (Switch) findViewById(R.id.switchOne);
+        Switch switchTwo = (Switch) findViewById(R.id.switchTwo);
+        Switch switchThree = (Switch) findViewById(R.id.switchThree);
+        Switch switchFour = (Switch) findViewById(R.id.switchFour);
+        Switch switchFive = (Switch) findViewById(R.id.switchFive);
 
-        buttonRequest.setOnClickListener(new View.OnClickListener() {
+
+        // check current state of a Switch (true or false).
+        Boolean switchOneChecked = switchOne.isChecked();
+
+        Boolean switchTwoChecked = switchTwo.isChecked();
+        Boolean switchThreeChecked = switchThree.isChecked();
+        Boolean switchFourChecked = switchFour.isChecked();
+        Boolean switchFiveChecked = switchFive.isChecked();
+
+
+        //Checked On Change Listener para Switch
+        switchOne.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             @Override
-            public void onClick(View view) {
-                if (ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED) {
+            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+                if (ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.READ_CONTACTS) == PackageManager.PERMISSION_GRANTED) {
                     Toast.makeText(MainActivity.this,"Ya concediste este permiso", Toast.LENGTH_LONG).show();
                 }
 
@@ -36,10 +55,12 @@ public class MainActivity extends AppCompatActivity {
                 }
             }
         });
+
+
     }
 
     private void requestStoragePermission(){
-        if (ActivityCompat.shouldShowRequestPermissionRationale(this,Manifest.permission.READ_EXTERNAL_STORAGE)) {
+        if (ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.READ_CONTACTS)) {
 
             new AlertDialog.Builder(this)
                     .setTitle("Permiso requerido")
@@ -48,8 +69,7 @@ public class MainActivity extends AppCompatActivity {
                         @Override
                         public void onClick(DialogInterface dialogInterface, int i) {
 
-                            ActivityCompat.requestPermissions(MainActivity.this,new String[]{
-                                    Manifest.permission.READ_EXTERNAL_STORAGE}, STORAGE_PERMISSION_CODE);
+                            ActivityCompat.requestPermissions(MainActivity.this,new String[]{Manifest.permission.READ_CONTACTS}, STORAGE_PERMISSION_CODE);
 
                         }
                     })
@@ -63,8 +83,7 @@ public class MainActivity extends AppCompatActivity {
         }
 
         else {
-            ActivityCompat.requestPermissions(this,new String[]{
-                    Manifest.permission.READ_EXTERNAL_STORAGE}, STORAGE_PERMISSION_CODE);
+            ActivityCompat.requestPermissions(this,new String[]{Manifest.permission.READ_CONTACTS}, STORAGE_PERMISSION_CODE);
         }
     }
 
